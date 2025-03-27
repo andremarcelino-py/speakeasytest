f3edd947ea2e37c23a408d45d172d49dfd95c428
andremarcelino-py <andremarcelinoemail@gmail.com>
2025-03-27 07:35:16 -0300

    Atualizar o firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCVGQk1Ctp1IZJrHQdM6YUSItaD3pypjg",
@@ -14,24 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-document.getElementById("start-button").addEventListener("click", async () => {
-  const name = document.getElementById("name").value;
-  const number = document.getElementById("number").value;
-
-  if (name && number) {
-    try {
-      await addDoc(collection(db, "users"), { name, number });
-
-      // Esconder o cadastro e mostrar o quiz
-      document.getElementById("register-container").style.display = "none";
-      document.getElementById("main-container").style.display = "block";
-    } catch (error) {
-      console.error("Erro ao salvar no Firestore: ", error);
-    }
-  } else {
-    alert("Preencha todos os campos!");
-
-  }
-});
-
-export { db }
+export { db };
